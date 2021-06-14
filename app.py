e32117b64162bd0f21089c2750eea108d44660ac
SabaAlex <sabadusalex@gmail.com>
2021-06-14 21:52:57 +0300

    Final Commit

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import functools
+from random import randint, random
+from tkinter import Radiobutton
 from algorithm.ea.evolutionaryAlgorithm import EvolutionaryAlgorithm
 from flask import Flask
 from flask import request
@@ -9,6 +11,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from apscheduler.schedulers.background import BackgroundScheduler
 from utils.atomic_counter import FastReadCounter
+
 import logging
 
 logging.basicConfig(filename='myapp.log', level=logging.INFO)
@@ -19,22 +22,42 @@ connection_counter = FastReadCounter()
 
 gaussNewtonAlgirithm = None
 
+vo2MAX = 4453
+
+hrMin = 52
+
+hrReading_error = 0.05
 
 xValues = []
 yValues = []
 hrReadings = []
 
+SQLALCHEMY_ENGINE_OPTIONS = {
+    "pool_pre_ping": True,
+    "pool_recycle": 300,
+}
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/server.db'
 socketio = SocketIO(app)
 db = SQLAlchemy(app)
+db.create_all()
 migrate = Migrate(app, db)
 
+class CoefficientData(db.Model):
+    __tablename__ = 'coefficients'
+    id = db.Column(db.Integer, primary_key=True)
+    coefficients = db.Column(db.String(120))
+    rating = db.Column(db.String(120))
+
+def compute_vo2_current(current_reading, error, hr_min, vo2_max):
+    return (((current_reading * (1 - error)) / hr_min * 15.3) / vo2_max) * 100
 
 def compute_hr_mean_last_seconds():
     try:
-        emit("get parameters")
-        yValues.append(functools.reduce(lambda a, b: (a * 0.75) + (b * 0.25), hrReadings))
+        emit("get parameters", {}, broadcast=True)
+        average_reading = functools.reduce(lambda a, b: (a * 0.75) + (b * 0.25), hrReadings)
+        yValues.append(compute_vo2_current(average_reading, hrReading_error, hrMin, vo2MAX))
         hrReadings.clear()
     except Exception:
         pass
@@ -43,15 +66,32 @@ def compute_hr_mean_last_seconds():
 scheduler = BackgroundScheduler()
 scheduler.add_job(func=compute_hr_mean_last_seconds, trigger="interval", seconds = 5)
 
+def load_data_from_db():
+    try:
+        result = CoefficientData.query.all()
+
+        DB_COEFFICIENTS = []
+        RATINGS = []
+
+        for row in result:
+            DB_COEFFICIENTS.append(map(lambda element: int(element), str(row[1]).split(',')))
+            RATINGS.append(int(row[2]))
+
+        coeff_size = len(DB_COEFFICIENTS)
+        DB_COEFFICIENTS = np.array(DB_COEFFICIENTS)
+
+        ACCUMULATOR = DB_COEFFICIENTS[0] * RATINGS[0]
+        for i in range(1, len(coeff_size)):
+            ACCUMULATOR = ACCUMULATOR + DB_COEFFICIENTS[i] * RATINGS[i]
+            
+        return ACCUMULATOR / sum(RATINGS)
+    except Exception:
+        return None
 
 #routes
 @app.route("/test")
 def testPage():
     logger.info("Loggint test run")
-    gauss_newton_coeff = [100, 0.1, 0.1, 2, 15]
-    gauss_newton_coeff = np.array(gauss_newton_coeff)
-    asd = GNSolver(gaussFunction, gauss_newton_coeff, np.array(xValues), np.array(yValues), tolerance_difference=5)
-    asd.log()
     return "Request Works"
 
 @app.route("/hrReading", methods=['POST'])
@@ -93,7 +133,15 @@ def end_calibration():
 @app.route("/init")
 def init_gauss_newton_algorithm():
 
-    gauss_newton_coeff = [100, 0.1, 0.1, 2, 15]
+    db_loaded = load_data_from_db()
+
+    np_x_Values = np.array(xValues)
+
+    gauss_newton_coeff = [randint(5, 50) for i in range(np_x_Values.shape[1])]
+
+    if db_loaded is not None:
+        gauss_newton_coeff = db_loaded
+
     gauss_newton_coeff = np.array(gauss_newton_coeff)
 
     gauss_newton_algirithm = GNSolver(gaussFunction, gauss_newton_coeff, np.array(xValues), np.array(yValues), tolerance_difference=5)
@@ -102,7 +150,11 @@ def init_gauss_newton_algorithm():
     while not isFit:
         isFit, new_coeff = gauss_newton_algirithm.fitNext()
 
-    emit("configured parameters", {"data" : new_coeff}, boradcast=True)
+    newData = CoefficientData(','.join(new_coeff), str(randint(1, 5)))
+    db.session.add(newData)
+    db.session.commit()
+
+    emit("configured parameters", {"data" : new_coeff}, broadcast=True)
     
 
 def gaussFunction(xValues : np.ndarray, coeff : list): 
@@ -125,7 +177,9 @@ def get_max_variables(coeff):
             sum_result += coeff[i] * x[i]
         return sum_result
 
-    ea_algorithm = EvolutionaryAlgorithm(xValues.shape[1], 500, 0.2, xValues.shape[0], 2, gauss_function, 95, xValues, 4)
+    np_x_Values = np.array(xValues)
+
+    ea_algorithm = EvolutionaryAlgorithm(np_x_Values.shape[1], 500, 0.2, np_x_Values.shape[0], 2, gauss_function, 95, np_x_Values, 4)
 
     fittest = ea_algorithm.runAlgorithm().get_list_of_variables()
 
@@ -140,7 +194,9 @@ def get_min_variabless(coeff):
             sum_result += coeff[i] * x[i]
         return sum_result
 
-    ea_algorithm = EvolutionaryAlgorithm(xValues.shape[1], 500, 0.2, xValues.shape[0], 2, gauss_function, 20, xValues, 4)
+    np_x_Values = np.array(xValues)
+
+    ea_algorithm = EvolutionaryAlgorithm(np_x_Values.shape[1], 500, 0.2, np_x_Values.shape[0], 2, gauss_function, 20, np_x_Values, 4)
 
     fittest = ea_algorithm.runAlgorithm().get_list_of_variables()
 
